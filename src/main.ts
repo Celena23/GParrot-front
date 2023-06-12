14e32ccbc2e326f59667c63a31552169b8ec79a0
piatce <ce.piat@sources-alma.com>
2023-06-12 16:09:34 +0200

    Init project

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Primevue, {usePrimeVue} from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import '../public/theme/celena/theme.css'
+
+import 'primeicons/primeicons.css';
+
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
 
 const app = createApp(App)
-
+app.use(Primevue)
 app.use(createPinia())
 app.use(router)
+app.component("InputText", InputText)
+app.component("Button", Button)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+import NavBar from "@/components/NavBar.vue";
+import Hero from "@/components/Hero.vue";
+import VehiculeList from "@/components/VehiculeList.vue";
+import Prestations from "@/components/Prestations.vue";
 </script>
 
 <template>
+  <NavBar/>
+  <Hero/>
+  <VehiculeList/>
+  <Button label="VOIR TOUS NOS VEHICULES" class="ml-3 p-button-outlined font-bold"></Button>
+  <div class="p-6">
+    <div class="border-2 border-black-alpha-90 surface-border border-round surface-card text-2xl line-height-4">
+    <p>
+      Nous assurons toutes les réparations mécaniques de votre véhicule, diagnostic, révision, entretien, réparation. <br>
+      Prise en charge toute panne sur tout véhicule.<br>
+      Toujours à votre écoute et à votre disposition, nous intervenons sur les organes mécaniques et électriques ou électroniques de votre véhicule.<br>
+      Une équipe de professionnels encadrés par un cotech qui assure les réparations et l'entretien de votre véhicule. Nous réalisons également le contrôle et le diagnostic des systèmes embarqués.
+    </p>
+    </div>
+  </div>
+<Prestations/>
   <main>
-    <TheWelcome />
   </main>
 </template>
